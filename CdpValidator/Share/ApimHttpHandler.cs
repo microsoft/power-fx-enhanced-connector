26a7b716bb38caf08513bebe67f54dc99e612784
Jim Daly <jim.daly@microsoft.com>
2025-06-20 14:40:35 -0700

    CdpValidator.ApimHttpHandler

--- CdpValidator/Share/ApimHttpHandler.cs
+++ CdpValidator/Share/ApimHttpHandler.cs
@@ -14,23 +14,34 @@ using Microsoft.PowerFx.Connectors;
 
 namespace CdpValidator
 {
-    // $$$ - move this into Fx.Connectors.
-
-    // Calls to APIM are via a relative URL.
-    // HttpClient will coerce to an absolute URL and then forward to the HttpMessageHandler.
-    // But our outgoing call to APIM needs to know the relative URL .
-    // invoke
-
+    /// <summary>
+    /// HTTP message handler for sending requests to APIM with Power Fx-specific headers and authentication.
+    /// </summary>
     internal class ApimHttpHandler : HttpMessageHandler
     {
+        /// <summary>
+        /// Gets or sets the APIM endpoint.
+        /// </summary>
         public string Endpoint { get; init; }
 
+        /// <summary>
+        /// Gets or sets the environment ID for the request.
+        /// </summary>
         public string EnvironmentId { get; init; }
 
+        /// <summary>
+        /// Gets or sets the connection ID for the request.
+        /// </summary>
         public string ConnectionId { get; init; }
 
+        /// <summary>
+        /// Gets or sets the function to retrieve the authentication token.
+        /// </summary>
         public Func<Task<string>> GetAuthToken { get; init; }
 
+        /// <summary>
+        /// Gets or sets the HTTP message invoker used to send the request.
+        /// </summary>
         public HttpMessageInvoker _invoker { get; init; }
 
         /// <summary>
@@ -38,26 +49,42 @@ namespace CdpValidator
         /// </summary>
         public string SessionId { get; set; } = Guid.NewGuid().ToString(); // "f4d37a97-f1c7-4c8c-80a6-f300c651568d"
 
+        /// <summary>
+        /// Gets the user agent string for telemetry.
+        /// </summary>
         public string UserAgent { get; }
 
         /// <summary>
-        /// For telemetry - assembly version stamp.
+        /// Gets the version of the PowerPlatformConnectorClient assembly for telemetry.
         /// </summary>
         public static string Version => typeof(PowerPlatformConnectorClient).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion.Split('+')[0];
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ApimHttpHandler"/> class.
+        /// </summary>
         public ApimHttpHandler()
         {
             string userAgent = null;
             this.UserAgent = string.IsNullOrWhiteSpace(userAgent) ? $"PowerFx/{Version}" : $"{userAgent} PowerFx/{Version}";
         }
 
+        /// <summary>
+        /// Sends an HTTP request asynchronously with APIM-specific headers and authentication.
+        /// </summary>
+        /// <param name="request">The HTTP request message to send.</param>
+        /// <param name="cancellationToken">A cancellation token to cancel operation.</param>
+        /// <returns>The HTTP response message.</returns>
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
             using HttpRequestMessage req = await Transform(request).ConfigureAwait(false);
             return await _invoker.SendAsync(request, cancellationToken).ConfigureAwait(false);
         }
 
-        // This will create a new request and send it. So the inner invoke can be another httpclient.
+        /// <summary>
+        /// Transforms the incoming request into a new request with APIM-specific headers and authentication.
+        /// </summary>
+        /// <param name="request">The original HTTP request message.</param>
+        /// <returns>A new <see cref="HttpRequestMessage"/> with the required headers and authentication.</returns>
         public async Task<HttpRequestMessage> Transform(HttpRequestMessage request)
         {
             var url = request.RequestUri.OriginalString;
@@ -70,8 +97,11 @@ namespace CdpValidator
                 }
                 else
                 {
-                    // Client has Basepath set.
-                    // x-ms-request-url needs relative URL.
+                    /* 
+                       Client has Basepath set.
+                       x-ms-request-url needs relative URL.
+                    */
+
                     throw new InvalidOperationException($"URL should be relative for x-ms-request-url property");
                 }
             }
@@ -92,8 +122,10 @@ namespace CdpValidator
             req.Headers.Add("x-ms-user-agent", UserAgent);
             req.Headers.Add("x-ms-request-url", url);
 
-            // might be needed for tabular connectors
-            //req.Headers.Add("X-Ms-Protocol-Semantics", "cdp");
+            /*
+                might be needed for tabular connectors
+                req.Headers.Add("X-Ms-Protocol-Semantics", "cdp");
+            */
 
             foreach (var header in request.Headers)
             {
